e65367b907554a32f21ab5d1e55a1d89810443d1
n0ah098099087 <148441037+n0ah098099087@users.noreply.github.com.>
2025-04-25 14:55:36 +0100

    movement system implimented with smooth rotation aswell as the bassis for the hideing system

--- Assets/Scripts/player_scripts/Movement_Script.cs
+++ Assets/Scripts/player_scripts/Movement_Script.cs
@@ -1,27 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.PlasticSCM.Editor.WebApi;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 
 
 public class Movement_Script : MonoBehaviour
 {
-
- public CharacterController controller;
- public float speed = 10f; 
+    public float Speed = 30f;
+    public float RotationSpeed = 360f;
+    float SprintSpeed;
+    float InitialSpeed;
+    float CurrentSpeed = 0;
+    bool Hidden;
+    Vector3 moveAbsolute;
+    Vector3 move;
+    Rigidbody rb;
+    public Transform Body;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        SprintSpeed = 3 * Speed;
+        InitialSpeed = Speed;
+        Hidden = false;
+
     }
 
     // Update is called once per frame
     void FixedUpdate ()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float Vertical = Input.GetAxisRaw("Vertical");
+        move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        rb.velocity = move * Speed;
+       
+        
 
+        if (move.x != 0 || move.z != 0)
+        {
+          Body.rotation = Quaternion.RotateTowards(Body.rotation, Quaternion.LookRotation(- move),RotationSpeed *Time.deltaTime);
+          
+        }
+
+        if (Input.GetButton("Sprint"))
+        {
+            Speed = SprintSpeed;
+            CurrentSpeed = SprintSpeed;
+
+        }
+        else
+        {
+            Speed = InitialSpeed;
+            
+        }
+      
     }
+    void OnTriggerEnter(Collider other)
+        {
+            Debug.Log("hidden");
+
+            if (other.gameObject.tag == "HidingPlace")
+            {
+                Hidden = true;
+                Debug.Log("hidden");
+            }
+        }
+
+      void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.tag == "HidingPlace")
+            {
+                Hidden = false;
+                Debug.Log("not hidden");
+            }
+        }
 }
